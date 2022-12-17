6537a69d5fda6f7dbe7cec8c00441a18a2df25d8
Yetman <yahiazakaria13@gmail.com>
2022-12-17 21:55:29 +0200

    Fixed paper experiments.

--- paper_experiments.ps1
+++ paper_experiments.ps1
@@ -1,6 +1,6 @@
-$RUNS = 3
+$RUNS = 5
 
-# This script runs 3 * 10 = 30 experiments so it will run for a REALLY LONG time.
+# This script runs 5 * 10 = 50 experiments so it will run for a REALLY LONG time.
 # It is meant to be used to replicate all the experiments in the paper.
 
 for($i = 1; $i -le $RUNS; $i++){
@@ -21,7 +21,7 @@ for($i = 1; $i -le $RUNS; $i++){
 
     Write-Output "${i}/${RUNS}: $GAME (Diversity Sampling: Tuple) ..."
     $PATH = ".\experiments\$GAME\DIV_GFLOW\${i}"
-    python .\cli.py train -cfg .\configs\$GAME\gflownet\div-tuple.yml -ovr save_path=$PATH dataset_config.property_reward="" dataset_config.data_augmentation:=False
+    python .\cli.py train -cfg .\configs\$GAME\gflownet\div-tuple.yml -ovr save_path=$PATH dataset_config.property_reward:=None dataset_config.data_augmentation:=False
     ./analyze.ps1 $GAME $PATH
 
     Write-Output "${i}/${RUNS}: $GAME (Diversity Sampling: Tuple + Property Reward) ..."
@@ -31,7 +31,7 @@ for($i = 1; $i -le $RUNS; $i++){
 
     Write-Output "${i}/${RUNS}: $GAME (Diversity Sampling: Tuple + Data Augmentation) ..."
     $PATH = ".\experiments\$GAME\DIV_AUG_GFLOW\${i}"
-    python .\cli.py train -cfg .\configs\$GAME\gflownet\div-tuple.yml -ovr save_path=$PATH dataset_config.property_reward=""
+    python .\cli.py train -cfg .\configs\$GAME\gflownet\div-tuple.yml -ovr save_path=$PATH dataset_config.property_reward:=None
     ./analyze.ps1 $GAME $PATH
 
     Write-Output "${i}/${RUNS}: $GAME (Diversity Sampling: Tuple + Property Reward + Data Augmentation) ..."
@@ -46,25 +46,25 @@ for($i = 1; $i -le $RUNS; $i++){
     Write-Output "${i}/${RUNS}: $GAME (Diversity Sampling: Tuple + Property Reward) ..."
     $PATH = ".\experiments\$GAME\DIV_PR_GFLOW\${i}"
     python .\cli.py train -cfg .\configs\$GAME\gflownet\config.yml -ovr save_path=$PATH dataset_config.data_augmentation:=False
-    ./analyze.ps1 $GAME $PATH
+    ./analyze.ps1 $GAME $PATH -TAILOREDGMM
 
     Write-Output "${i}/${RUNS}: $GAME (Diversity Sampling: Tuple + Property Reward + Data Augmentation) ..."
     $PATH = ".\experiments\$GAME\DIV_PR_AUG_GFLOW\${i}"
     python .\cli.py train -cfg .\configs\$GAME\gflownet\config.yml -ovr save_path=$PATH
-    ./analyze.ps1 $GAME $PATH
+    ./analyze.ps1 $GAME $PATH -TAILOREDGMM
 
     # Danger Dave Experiments
 
     $GAME = "dave"
 
     Write-Output "${i}/${RUNS}: $GAME (Diversity Sampling: Tuple) ..."
-    $PATH = ".\experiments\$GAME\DIV_PR_GFLOW\${i}"
+    $PATH = ".\experiments\$GAME\DIV_GFLOW\${i}"
     python .\cli.py train -cfg .\configs\$GAME\gflownet\config.yml -ovr save_path=$PATH dataset_config.data_augmentation:=False
-    ./analyze.ps1 $GAME $PATH
+    ./analyze.ps1 $GAME $PATH -TAILOREDGMM
 
     Write-Output "${i}/${RUNS}: $GAME (Diversity Sampling: Tuple + Data Augmentation) ..."
-    $PATH = ".\experiments\$GAME\DIV_PR_AUG_GFLOW\${i}"
+    $PATH = ".\experiments\$GAME\DIV_AUG_GFLOW\${i}"
     python .\cli.py train -cfg .\configs\$GAME\gflownet\config.yml -ovr save_path=$PATH
-    ./analyze.ps1 $GAME $PATH
+    ./analyze.ps1 $GAME $PATH -TAILOREDGMM
 
 }
